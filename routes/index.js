3599cb3b8e3b7cafcfe9244645c1387cc4717f3e
Wesonga Bob <31077716+wesonga@users.noreply.github.com>
2023-07-12 09:42:49 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'GOLDEN TIME' });
+  res.render('index', { title: 'UBUNTU KWANZAA' });
 });
 
 module.exports = router;
